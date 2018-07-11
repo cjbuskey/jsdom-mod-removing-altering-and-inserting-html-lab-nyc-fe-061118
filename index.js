7d9f5f6dda8a4aa8adceefb6e9d76090fec14695
clark.buskey@gmail.com <clark.buskey@gmail.com>
2018-07-11 23:59:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 /* Enter the code to remove the main node element under this comment */
 main.remove()
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
-var element = createElement('h1', victory)
-element.innerHTML = 'CJ is the champion!'
-document.body.
+const newHeader = createElement('h1', victory)
+newHeader.innerHTML = 'CJ is the champion!'
