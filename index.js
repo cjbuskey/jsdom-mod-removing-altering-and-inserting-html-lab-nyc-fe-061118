8f16c7a220a44c6c519c98ab523fda1240cde882
clark.buskey@gmail.com <clark.buskey@gmail.com>
2018-07-12 00:03:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /* Enter the code to remove the main node element under this comment */
 main.remove()
 /* Create your new element here and assign it to newHeader */
-const newHeader = document.createElement('h1', victory);
+const newHeader = document.createElement('h1');
 newHeader.innerHTML = 'CJ is the champion!';
